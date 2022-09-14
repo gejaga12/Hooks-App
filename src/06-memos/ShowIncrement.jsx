056d01bf3bee9b6bcf66fea20d1af4e69984a090
Gerardo Javier García <gejaga12@hotmail.com>
2022-09-14 11:27:43 -0300

    aplico useCallback a funciones del hijo

--- src/06-memos/ShowIncrement.jsx
+++ src/06-memos/ShowIncrement.jsx
@@ -4,7 +4,7 @@ export const ShowIncrement = React.memo(({ increment }) => {
   console.log("Me volvi a generar");
 
   return (
-    <button className="btn btn-danger" onClick={() => increment()}>
+    <button className="btn btn-danger" onClick={(value) => increment(5)}>
       Incrementar
     </button>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { CallbackHook } from "./06-memos/CallbackHook";
+import { Padre } from "./07-tarea-memo/Padre";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import CounterApp from './01-useState/CounterApp'
@@ -15,6 +16,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <CallbackHook />
+  <Padre />
   // </React.StrictMode>
 );
