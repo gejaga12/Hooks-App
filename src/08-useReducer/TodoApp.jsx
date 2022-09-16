e4228d373e16be8eeb9c4250dc60887b41f6abb2
Gerardo Javier García <gejaga12@hotmail.com>
2022-09-16 08:29:52 -0300

    agrego onDeleteTodo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -30,6 +30,13 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
+
   return (
     <>
       <h1>
@@ -39,7 +46,7 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos} />
+          <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
         </div>
         <div className="col-5">
           <h4>Agregar Tarea</h4>
